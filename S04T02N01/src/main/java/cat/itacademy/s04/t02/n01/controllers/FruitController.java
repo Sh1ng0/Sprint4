5adec3b71b222e5f96ffd0f22b31ca4cbb559b62
Josiko <reye31416@gmail.com>
2024-11-27 13:05:15 +0100

    Hola Arnau!!1 (╯°□°)╯︵ ┻━┻

--- S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/controllers/FruitController.java
+++ S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/controllers/FruitController.java
@@ -3,7 +3,10 @@ package cat.itacademy.s04.t02.n01.controllers;
 
 import cat.itacademy.s04.t02.n01.model.Fruit;
 import cat.itacademy.s04.t02.n01.services.FruitService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,33 +17,68 @@ import java.util.List;
 
 public class FruitController {
 
+
+    @Autowired
     private final FruitService fruitService;
 
 
-    @Autowired
     public FruitController(FruitService fruitService) {
         this.fruitService = fruitService;
     }
 
     @GetMapping
-    public List<Fruit> getAllFruits() {
-        return fruitService.getAllFruits();
+    public ResponseEntity<List<Fruit>> getAllFruits() {
+
+        try {
+            List<Fruit> fruites = fruitService.getAllFruits();
+            if (fruites.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+            }
+            return new ResponseEntity<>(fruites, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
-    @PostMapping
-    public Fruit addFruit(@RequestBody Fruit fruit) {
-        return fruitService.addFruit(fruit);
+    @PostMapping("/add")
+    public ResponseEntity<Fruit> addFruit(@RequestBody Fruit fruit) {
+
+
+        try {
+            Fruit newFruit = fruitService.addFruit(fruit);
+            return new ResponseEntity<>(newFruit, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
     }
 
     @GetMapping("/{id}")
-    public Fruit getFruitById(@PathVariable int id) {
-        return fruitService.getFruitById(id);
+    public ResponseEntity<Fruit> getFruitById(@PathVariable int id) {
+        try {
+            Fruit fruit = fruitService.getFruitById(id);
+            return new ResponseEntity<>(fruit, HttpStatus.OK);
+
+        } catch(Exception e){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+
     }
 
     @DeleteMapping("/{id}")
-    public void deleteFruitById(@PathVariable int id) {
-        fruitService.deleteFruitById(id);
+    public ResponseEntity<HttpStatus> deleteFruitById(@PathVariable int id) {
+
+
+        try {
+            fruitService.deleteFruitById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
